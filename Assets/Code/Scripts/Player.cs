8643f10bca967629a0a4c4f4af4c28f1dc8e65e3
Gyeom Chong <kenzzang1@kaist.ac.kr>
2024-10-29 23:35:54 +0900

    Change class name (Character -> Player)

--- Assets/Code/Scripts/Player.cs
+++ Assets/Code/Scripts/Player.cs
@@ -1,8 +1,8 @@
 ﻿namespace Code.Scripts
 {
-    public class Character
+    public class Player
     {
-        private int _atk; // 공격력
+	    private int _atk; // 공격력
 		private int _hp; //체력
 		private int _lv; // 레벨
 		private int _exp; // 경험치
