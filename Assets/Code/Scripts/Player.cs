750e6b736225b2d85d533a89463e79f9eaeb196e
Gyeom Chong <kenzzang1@kaist.ac.kr>
2024-10-30 23:32:27 +0900

    Change trivial variable name (HP -> Hp)

--- Assets/Code/Scripts/Player.cs
+++ Assets/Code/Scripts/Player.cs
@@ -5,7 +5,7 @@ namespace MJ.Player
     public class Player : MonoBehaviour
     {
 	    private float _playerAttackPower; // 공격력
-		private float _playerHP; //체력
+		private float _playerHp; //체력
 		private int _playerLevel; // 레벨
 		private float _playerExp; // 경험치
 		private float _playerMoveSpeed; // 이동 속도
