456faaa56115483c9c8f1219f555f6cce2c25614
Gyeom Chong <kenzzang1@kaist.ac.kr>
2024-11-01 14:43:53 +0900

    Add playerstat property

--- Assets/Code/Scripts/Player.cs
+++ Assets/Code/Scripts/Player.cs
@@ -4,33 +4,40 @@ namespace MJ.Player
 {
     public class Player : MonoBehaviour
     {
-	    private float _playerAttackPower; // 공격력
-		private float _playerHp; //체력
-		private int _playerLevel; // 레벨
-		private float _playerExp; // 경험치
-		private float _playerMoveSpeed; // 이동 속도
+        private float _playerAttackPower; // 공격력
+        private float _playerHp; //체력
+        private int _playerLevel; // 레벨
+        private float _playerExp; // 경험치
+        private float _playerMoveSpeed; // 이동 속도
 
-		public PlayerStat playerStat;
-		
-		private void InitializePlayerStats()
-		{
-			_playerHp = playerStat.playerHp;
-			_playerAttackPower = playerStat.playerAttackPower;
-			_playerLevel = playerStat.playerLevel;
-			_playerExp = playerStat.playerExp;
-			_playerMoveSpeed = playerStat.playerMoveSpeed;
-		}
-		
-		private void OnEnable()
-		{
-			// 게임 시작 이벤트 구독
-			GameEvents.OnGameStart.AddListener(InitializePlayerStats);
-		}
+        public PlayerStat playerStat;
 
-		private void OnDisable()
-		{
-			// 게임 시작 이벤트 구독 해제
-			GameEvents.OnGameStart.RemoveListener(InitializePlayerStats);
-		}
+        // 플레이어 스탯 관련 프로퍼티
+        public float playerAttackPower { get; set; }
+        public float playerHp { get; set; }
+        public int playerLevel { get; set; }
+        public float playerExp { get; set; }
+        public float playerMoveSpeed { get; set; }
+
+        private void InitializePlayerStats()
+        {
+            _playerHp = playerStat.playerHp;
+            _playerAttackPower = playerStat.playerAttackPower;
+            _playerLevel = playerStat.playerLevel;
+            _playerExp = playerStat.playerExp;
+            _playerMoveSpeed = playerStat.playerMoveSpeed;
+        }
+
+        private void OnEnable()
+        {
+            // 게임 시작 이벤트 구독
+            GameEvents.OnGameStart.AddListener(InitializePlayerStats);
+        }
+
+        private void OnDisable()
+        {
+            // 게임 시작 이벤트 구독 해제
+            GameEvents.OnGameStart.RemoveListener(InitializePlayerStats);
+        }
     }
 }
