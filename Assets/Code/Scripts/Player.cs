8512607489cc144c48af5819a9697ffc72a7e79a
Gyeom Chong <kenzzang1@kaist.ac.kr>
2024-11-01 14:32:29 +0900

    Add GameStart event, PlayerStat settings

--- Assets/Code/Scripts/Player.cs
+++ Assets/Code/Scripts/Player.cs
@@ -9,5 +9,28 @@ namespace MJ.Player
 		private int _playerLevel; // 레벨
 		private float _playerExp; // 경험치
 		private float _playerMoveSpeed; // 이동 속도
+
+		public PlayerStat playerStat;
+		
+		private void InitializePlayerStats()
+		{
+			_playerHp = playerStat.playerHp;
+			_playerAttackPower = playerStat.playerAttackPower;
+			_playerLevel = playerStat.playerLevel;
+			_playerExp = playerStat.playerExp;
+			_playerMoveSpeed = playerStat.playerMoveSpeed;
+		}
+		
+		private void OnEnable()
+		{
+			// 게임 시작 이벤트 구독
+			GameEvents.OnGameStart.AddListener(InitializePlayerStats);
+		}
+
+		private void OnDisable()
+		{
+			// 게임 시작 이벤트 구독 해제
+			GameEvents.OnGameStart.RemoveListener(InitializePlayerStats);
+		}
     }
 }
