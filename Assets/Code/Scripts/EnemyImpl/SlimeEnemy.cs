35ea5a07c1f99e1c469b0f184473da1aaf4512f6
Daegyeom Ha <combbm@gmail.com>
2024-11-02 14:23:28 +0900

    feat(enemy): SlimeEnemy 코드 추가

---
+++ Assets/Code/Scripts/EnemyImpl/SlimeEnemy.cs
@@ -0,0 +1,9 @@
+﻿namespace MJ.Enemy
+{
+    public class SlimeEnemy : Enemy
+    {
+        private float _enemyAttackPower = 5.0f;
+        private float _enemyHp = 20.0f;
+        private float _enemyMoveSpeed = 2.0f;
+    }
+}
