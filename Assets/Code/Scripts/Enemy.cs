63316f8fb0a548fa5bc4c8efc4d72ab19b201e02
Daegyeom Ha <combbm@gmail.com>
2024-11-02 14:09:19 +0900

    style(enemy): 변수 이름 수정

--- Assets/Code/Scripts/Enemy.cs
+++ Assets/Code/Scripts/Enemy.cs
@@ -5,7 +5,7 @@ namespace MJ.Enemy
     public class Enemy : MonoBehaviour
     {
         private float _enemyAttackPower; // 적 공격력
-        private float _enemyHP; // 적 체력
+        private float _enemyHp; // 적 체력
         private float _enemyMoveSpeed; // 적 이동속도
     }
 }
