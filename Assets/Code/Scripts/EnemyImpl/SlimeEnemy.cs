462651380a0b77f3bd992c4248279f1dfd631f97
Daegyeom Ha <combbm@gmail.com>
2024-11-02 14:42:52 +0900

    feat(enemy): 적이 죽었을 때 행위 구현

--- Assets/Code/Scripts/EnemyImpl/SlimeEnemy.cs
+++ Assets/Code/Scripts/EnemyImpl/SlimeEnemy.cs
@@ -5,5 +5,14 @@
         private float _enemyAttackPower = 5.0f;
         private float _enemyHp = 20.0f;
         private float _enemyMoveSpeed = 2.0f;
+
+        private void Update()
+        {
+            var isEnemyDead = _enemyHp <= 0;
+            if (isEnemyDead)
+            {
+                killEnemy();
+            }
+        }
     }
 }
