47c6a2b8c74e46b4bc38cc5b774ddc94e187ed56
Daegyeom Ha <combbm@gmail.com>
2024-11-06 11:28:14 +0900

    fix(enemy): 적이 죽을 때 프리팹을 삭제하지 않고 비활성화 처리

--- Assets/Code/Scripts/Enemy.cs
+++ Assets/Code/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Code.Scripts;
+using UnityEngine;
 
 namespace MJ.Enemy
 {
@@ -7,11 +8,11 @@ namespace MJ.Enemy
         private float _enemyAttackPower; // 적 공격력
         private float _enemyHp; // 적 체력
         private float _enemyMoveSpeed; // 적 이동속도
-        
+
         public void killEnemy()
         {
-            Destroy(gameObject);
-            // 적 죽음 이벤트 발행
+            SpawnEnemyManager.ReturnToPool(this);
+            // TODO: 적 죽음 이벤트 발행
         }
     }
 }
