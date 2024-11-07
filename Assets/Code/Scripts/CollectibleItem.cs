22720b5154d40ecd5ba7643f03c200e9dd7b5fe4
Gyeom Chong <kenzzang1@kaist.ac.kr>
2024-11-07 23:17:14 +0900

    add collectibleitem collect code

--- Assets/Code/Scripts/CollectibleItem.cs
+++ Assets/Code/Scripts/CollectibleItem.cs
@@ -5,6 +5,13 @@ namespace MJ.CollectibleItem
 {
     public class CollectibleItem : MonoBehaviour
     {
-        private CollectibleItemTypes _collectibleItemType; // 타입
+        public CollectibleItemTypes _collectibleItemType {get; set;} // 타입
+
+        // 아이템이 수집될 때 호출되는 메서드
+        public void Collect()
+        {
+            // 수집 처리 로직 (예: 플레이어의 경험치 증가, 재화 증가 등) : todo
+            Destroy(gameObject);
+        }
     }
 }
