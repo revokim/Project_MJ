ce05069634475d5b24c04071739775aa510e96f6
ChocoThief <jbl6938@hanyang.ac.kr>
2024-11-13 00:26:22 +0900

    GameManager: 싱글톤화, 플레이어 변수 저장, 경험치 획득 함수 및 레벨업 구조

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -1,9 +1,27 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace MJ
 {
     public class GameManager : MonoBehaviour
     {
+        
+        public static GameManager Instance;
+        [Header("Game Control")]
+        public float gameTime;
+        
+        [Header("Player Info")]
+        public int level;
+        public int exp;
+        public int[] nextExp = {10, 30 ,60, 100, 150, 210, 280, 360, 450, 600};
+
+        [Header("Game Object")]
+        public Player.Player player; //풀레이어 게임오브젝트는 게임매니저를 통해 가져오기
+        private void Awake()
+        {
+            Instance = this;
+        }
+
         private void Start() // 인게임 시작 시 호출
         {
             StartGame();
@@ -12,6 +30,17 @@ namespace MJ
         {
             // 게임 시작 이벤트 호출
             GameEvents.TriggerGameStart();
+        }
+
+        public void GetExp()
+        {
+            exp++;
+            
+            if (exp == nextExp[level])
+            {
+                level++;
+                exp = 0;
+            }
         }
     }
 }
